39bb4069271d8c3bbf56c3b5103f09cf759eff6f
Victor Miene <Viktormiene.vm@gmail.com>
2021-02-11 23:34:18 +0100

    added pls test route to appjs

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.get('/protected', requireAuth)
+app.get('/works',(req,res) => {
+    res.send('dklnsd.kfnk.nd')
+})
 app.listen(process.env.PORT, () => {
     console.log('app running on ' + process.env.PORT)
 })
