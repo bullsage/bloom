40eebc8e66769256a8b6b227f778a99f1e43df58
Victor Miene <Viktormiene.vm@gmail.com>
2021-02-12 22:18:16 +0100

    added cors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+var cors = require('cors')
 const authRoutes = require('./routes/authRoutes')
 const adminRoutes = require('./routes/adminRoutes')
 const cookieParser = require('cookie-parser')
@@ -7,12 +8,11 @@ const {requireAuth} = require('./middleware/authMiddleware')
 require('dotenv').config()
 
 const app = express()
+
+app.use(cors())
 app.use(express.json())
 app.use(cookieParser())
 app.get('/protected', requireAuth)
-app.get('/works',(req,res) => {
-    res.send('dklnsd.kfnk.nd')
-})
 app.listen(process.env.PORT, () => {
     console.log('app running on ' + process.env.PORT)
 })
