7248650938b781ef9f71f088cb1f1a005e4e3fdf
Victor Miene <Viktormiene.vm@gmail.com>
2021-02-12 09:18:39 +0100

    adding config for knex

--- dbconfig.js
+++ dbconfig.js
@@ -2,10 +2,8 @@ require('dotenv').config()
 const db = require('knex')({
     client: 'pg',
     connection: {
-      host : process.env.HOST,
-      user : process.env.USER,
-      password : process.env.PASSWORD,
-      database : process.env.DATABASE
+      host : process.env.DATABASE_URL,
+      ssl: true
     }
 });
 
