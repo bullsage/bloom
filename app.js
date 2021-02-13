2980c04b9d40a91c0237541ad77bdc189671aea3
Victor Miene <Viktormiene.vm@gmail.com>
2021-02-13 17:41:29 +0100

    changed protected route to post

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express()
 app.use(express.json())
 // app.use(cookieParser())
 app.use(cors())
-app.get('/protected', requireAuth)
+app.post('/protected', requireAuth)
 app.listen(process.env.PORT, () => {
     console.log('app running on ' + process.env.PORT)
 })
